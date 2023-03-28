ff8094bdfeaed81d31bd564a95e6dc9476677ce7
Arve Seljebu <arve@seljebu.no>
2023-03-28 13:44:31 +0200

    remove product check

--- elasticsearch.go
+++ elasticsearch.go
@@ -21,7 +21,6 @@ import (
 	"encoding/base64"
 	"errors"
 	"fmt"
-	"github.com/elastic/go-elasticsearch/v8/typedapi"
 	"net/http"
 	"net/url"
 	"os"
@@ -31,6 +30,8 @@ import (
 	"strings"
 	"sync"
 	"time"
+
+	"github.com/elastic/go-elasticsearch/v8/typedapi"
 
 	"github.com/elastic/go-elasticsearch/v8/esapi"
 	"github.com/elastic/go-elasticsearch/v8/internal/version"
@@ -65,7 +66,6 @@ func init() {
 }
 
 // Config represents the client configuration.
-//
 type Config struct {
 	Addresses []string // A list of Elasticsearch nodes to use.
 	Username  string   // Username for HTTP Basic Authentication.
@@ -88,8 +88,8 @@ type Config struct {
 	MaxRetries    int                             // Default: 3.
 	RetryOnError  func(*http.Request, error) bool // Optional function allowing to indicate which error should be retried. Default: nil.
 
-	CompressRequestBody bool // Default: false.
-	CompressRequestBodyLevel int // Default: gzip.DefaultCompression.
+	CompressRequestBody      bool // Default: false.
+	CompressRequestBodyLevel int  // Default: gzip.DefaultCompression.
 
 	DiscoverNodesOnStart  bool          // Discover nodes when initializing the client. Default: false.
 	DiscoverNodesInterval time.Duration // Discover nodes periodically. Default: disabled.
@@ -111,7 +111,6 @@ type Config struct {
 }
 
 // BaseClient represents the Elasticsearch client.
-//
 type BaseClient struct {
 	Transport           elastictransport.Interface
 	metaHeader          string
@@ -140,7 +139,6 @@ type TypedClient struct {
 //
 // It will use the ELASTICSEARCH_URL environment variable, if set,
 // to configure the addresses; use a comma to separate multiple URLs.
-//
 func NewDefaultClient() (*Client, error) {
 	return NewClient(Config{})
 }
@@ -156,7 +154,6 @@ func NewDefaultClient() (*Client, error) {
 // environment variable is ignored.
 //
 // It's an error to set both cfg.Addresses and cfg.CloudID.
-//
 func NewClient(cfg Config) (*Client, error) {
 	tp, err := newTransport(cfg)
 	if err != nil {
@@ -275,7 +272,7 @@ func newTransport(cfg Config) (*elastictransport.Client, error) {
 		MaxRetries:    cfg.MaxRetries,
 		RetryBackoff:  cfg.RetryBackoff,
 
-		CompressRequestBody: cfg.CompressRequestBody,
+		CompressRequestBody:      cfg.CompressRequestBody,
 		CompressRequestBodyLevel: cfg.CompressRequestBodyLevel,
 
 		EnableMetrics:     cfg.EnableMetrics,
@@ -296,7 +293,6 @@ func newTransport(cfg Config) (*elastictransport.Client, error) {
 }
 
 // Perform delegates to Transport to execute a request and return a response.
-//
 func (c *BaseClient) Perform(req *http.Request) (*http.Response, error) {
 	// Compatibility Header
 	if c.compatibilityHeader {
@@ -318,58 +314,10 @@ func (c *BaseClient) Perform(req *http.Request) (*http.Response, error) {
 	}
 
 	// Retrieve the original request.
-	res, err := c.Transport.Perform(req)
-
-	// ResponseCheck, we run the header check on the first answer from ES.
-	if err == nil && (res.StatusCode >= 200 && res.StatusCode < 300) {
-		checkHeader := func() error { return genuineCheckHeader(res.Header) }
-		if err := c.doProductCheck(checkHeader); err != nil {
-			res.Body.Close()
-			return nil, err
-		}
-	}
-
-	return res, err
-}
-
-// doProductCheck calls f if there as not been a prior successful call to doProductCheck,
-// returning nil otherwise.
-func (c *BaseClient) doProductCheck(f func() error) error {
-	c.productCheckMu.RLock()
-	productCheckSuccess := c.productCheckSuccess
-	c.productCheckMu.RUnlock()
-
-	if productCheckSuccess {
-		return nil
-	}
-
-	c.productCheckMu.Lock()
-	defer c.productCheckMu.Unlock()
-
-	if c.productCheckSuccess {
-		return nil
-	}
-
-	if err := f(); err != nil {
-		return err
-	}
-
-	c.productCheckSuccess = true
-
-	return nil
-}
-
-// genuineCheckHeader validates the presence of the X-Elastic-Product header
-//
-func genuineCheckHeader(header http.Header) error {
-	if header.Get("X-Elastic-Product") != "Elasticsearch" {
-		return errors.New(unknownProduct)
-	}
-	return nil
+	return c.Transport.Perform(req)
 }
 
 // Metrics returns the client metrics.
-//
 func (c *BaseClient) Metrics() (elastictransport.Metrics, error) {
 	if mt, ok := c.Transport.(elastictransport.Measurable); ok {
 		return mt.Metrics()
@@ -378,7 +326,6 @@ func (c *BaseClient) Metrics() (elastictransport.Metrics, error) {
 }
 
 // DiscoverNodes reloads the client connections by fetching information from the cluster.
-//
 func (c *BaseClient) DiscoverNodes() error {
 	if dt, ok := c.Transport.(elastictransport.Discoverable); ok {
 		return dt.DiscoverNodes()
@@ -388,7 +335,6 @@ func (c *BaseClient) DiscoverNodes() error {
 
 // addrsFromEnvironment returns a list of addresses by splitting
 // the ELASTICSEARCH_URL environment variable with comma, or an empty list.
-//
 func addrsFromEnvironment() []string {
 	var addrs []string
 
@@ -403,7 +349,6 @@ func addrsFromEnvironment() []string {
 }
 
 // addrsToURLs creates a list of url.URL structures from url list.
-//
 func addrsToURLs(addrs []string) ([]*url.URL, error) {
 	var urls []*url.URL
 	for _, addr := range addrs {
@@ -419,7 +364,6 @@ func addrsToURLs(addrs []string) ([]*url.URL, error) {
 
 // addrFromCloudID extracts the Elasticsearch URL from CloudID.
 // See: https://www.elastic.co/guide/en/cloud/current/ec-cloud-id.html
-//
 func addrFromCloudID(input string) (string, error) {
 	var scheme = "https://"
 
